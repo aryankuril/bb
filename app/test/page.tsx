751c10b89e3e6c4d2fdade6d23b9e7d850e98ec1
Aryan Kuril <aryan@bombayblokes.com>
2025-10-06 17:11:16 +0530

    font test

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import Button from '../components/Button'
+import Navbar from '../components/Navbar'
 
 const Index = () => {
   return (
+    <div className="">
+    <Navbar/>
 <section className="w-full container py-10 sm:py-15 lg:py-20 flex flex-col md:flex-row items-center justify-between gap-10">
   {/* Left Side - Text */}
   <div className="flex-1 text-center md:text-left space-y-4">
@@ -10,7 +13,7 @@ const Index = () => {
       We Don’t Just Brand, <span className="text-highlight">We Build Icons.</span>
     </h1>
 
-    <p className="body2 w-full max-w-[1000px] mx-auto  mt-4 break-words">
+    <p className="body2 w-full max-w-[1000px]  mt-4 break-words">
       From Strategy To Storytelling, Identity To Impact, We Craft Everything Your Brand Needs To Stand Out And Grow.
     </p>
 
@@ -21,6 +24,7 @@ const Index = () => {
 
 
 </section>
+</div>
   )
 }
 
