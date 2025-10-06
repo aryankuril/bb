f19b451cd05b1497188ca8575e7fdc7ec24a9053
Aryan Kuril <aryan@bombayblokes.com>
2025-10-06 17:28:13 +0530

    font test

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -9,9 +9,9 @@ const Index = () => {
 <section className="w-full container py-10 sm:py-15 lg:py-20 flex flex-col md:flex-row items-center justify-between gap-10">
   {/* Left Side - Text */}
   <div className="flex-1 text-center md:text-left space-y-4">
-    <h1 className= "w-full max-w-[650px] mx-auto md:mx-0 lg:mt-10">
+    <div className= " text-[80px] w-full font['Poppins'] max-w-[650px] mx-auto md:mx-0 lg:mt-10">
       We Don’t Just Brand, <span className="text-highlight">We Build Icons.</span>
-    </h1>
+    </div>
 
     <p className="body2 w-full max-w-[1000px]  mt-4 break-words">
       From Strategy To Storytelling, Identity To Impact, We Craft Everything Your Brand Needs To Stand Out And Grow.
