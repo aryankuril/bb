37dbfa1707bf3b4b992a73ad0e6701143eff84fb
Aryan Kuril <aryan@bombayblokes.com>
2025-10-11 04:16:56 +0530

    team page hide

--- app/teams/page.tsx
+++ app/teams/page.tsx
@@ -1,23 +1,29 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
-import Firstsection from '@/app/components/Teams/Firstsection'
-// import SecondSection from '@/app/components/Teams/SecondSection'
-import ThirdSection from '@/app/components/Teams/ThirdSection'
-import SeventhSection from '../components/ServicesInternal/WebsiteDesign/SeventhSection'
-import Footer from '../components/Footer'
-import ClientScripts from '../components/ClientScripts'
+// import Navbar from '../components/Navbar'
+// import Firstsection from '@/app/components/Teams/Firstsection'
+// // import SecondSection from '@/app/components/Teams/SecondSection'
+// import ThirdSection from '@/app/components/Teams/ThirdSection'
+// import SeventhSection from '../components/ServicesInternal/WebsiteDesign/SeventhSection'
+// import Footer from '../components/Footer'
+// import ClientScripts from '../components/ClientScripts'
 
 
 const Index = () => {
   return (
     <div>
-      <ClientScripts/>
+      {/* <ClientScripts/>
         <Navbar />
         <Firstsection />
         {/* <SecondSection /> */}
-        <ThirdSection />
+        {/* <ThirdSection />
         <SeventhSection />
-        <Footer />
+        <Footer /> */} 
+
+        <div className="flex items-center justify-center min-h-screen bg-white text-black">
+      <h1 className="text-2xl sm:text-4xl font-semibold text-center px-4">
+        This page is under construction, we’ll fix it soon 🚧
+      </h1>
+    </div>
     </div>
   )
 }
