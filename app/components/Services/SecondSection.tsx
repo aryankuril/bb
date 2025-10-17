f6b4aa4fb6161ec8964e8cebe11fa24c0d3511d7
Aryan Kuril <aryan@bombayblokes.com>
2025-10-17 15:15:15 +0530

    home page video added

--- app/components/Services/SecondSection.tsx
+++ app/components/Services/SecondSection.tsx
@@ -19,7 +19,7 @@ const cards: Card[] = [
     subtitle:
       "We build pixel-perfect websites and digital experiences that aren't just beautiful, but are engineered to convert.",
     image: "/images/servicespage/Development.png",
-    link: "/website-design",
+    link: "/services/website-design",
     shape: "rectangle",
   },
   {
@@ -27,7 +27,7 @@ const cards: Card[] = [
     subtitle:
       "We create data-driven ad campaigns that deliver measurable results, turning clicks into customers and spend into revenue.",
     image: "/images/servicespage/Performance.png",
-    link: "/performance-maketing",
+    link: "/services/performance-maketing",
     shape: "square",
   },
   {
@@ -35,7 +35,7 @@ const cards: Card[] = [
     subtitle:
       "We put your brand at the top of Google, connecting you with customers who are already searching for you.",
     image: "/images/servicespage/SEO.png",
-    link: "/seo",
+    link: "/services/seo",
     shape: "circle",
   },
   {
@@ -43,7 +43,7 @@ const cards: Card[] = [
     subtitle:
       "We build and nurture your online community, turning followers into loyal fans through creative content and authentic engagement.",
     image: "/images/servicespage/Social-Media.png",
-    link: "/social-media",
+    link: "/services/social-media",
     shape: "rectangle",
   },
 ];
---
+++ app/services/website-design/page.tsx
@@ -0,0 +1,31 @@
+import Navbar from "../../components/Navbar";
+import FirstSection from "../../components/ServicesInternal/WebsiteDesign/FirstSection";
+// import SecondSection from "../components/ServicesInternal/WebsiteDesign/SecondSection";
+import ThirdSection from "../../components/ServicesInternal/WebsiteDesign/ThirdSection";
+import FourthSection from "../../components/ServicesInternal/WebsiteDesign/FourthSection";
+import FifthSection from "../../components/ServicesInternal/WebsiteDesign/FifthSection";
+import SixthSection from "../../components/ServicesInternal/WebsiteDesign/SixthSection";
+import SeventhSection from "../../components/ServicesInternal/WebsiteDesign/SeventhSection";
+import Footer from "../../components/Footer";
+// import ClientScripts from '../components/ClientScripts'
+import Taxi from '../../components/Taxi'
+
+const Index = () => {
+  return (
+    <div>
+      {/* <ClientScripts/> */}
+      <Taxi/>
+      <Navbar />
+      <FirstSection />
+      {/* <SecondSection /> */}
+      <FourthSection />
+      <ThirdSection />
+      <FifthSection />
+      <SixthSection />
+      <SeventhSection />
+      <Footer />
+    </div>
+  )
+}
+
+export default Index
