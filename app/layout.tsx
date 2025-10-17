c2f3451aad376ca21e2b6f23e5866e4ef5a36500
Aryan Kuril <aryan@bombayblokes.com>
2025-10-17 19:27:45 +0530

    tracking added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 import Taxi from "./components/Taxi";
 import PageLoader from "./components/PageLoader";
 import ScrollToTop from "./components/ScrollToTop";
@@ -35,21 +36,37 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={`${miso.variable} ${poppins.variable}`}>
+   <html lang="en" className={`${miso.variable} ${poppins.variable}`}>
       <head>
+        <meta name="google-site-verification" content="vcgYWAz5xbO_xhFBzKSTAJuBzaum2orDl7K2CaoMTPw" />
         <link rel="icon" href="images/favicon.png" type="image/png" />
       </head>
+
+      {/* ✅ Add Google Analytics safely using next/script */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-E86THEZTKX"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-E86THEZTKX');
+        `}
+      </Script>
+
       <body>
-        <SmoothScroll>  
-         <PageLoader>
-           {children}
+        <SmoothScroll>
+          <PageLoader>
+            {children}
             {/* <Taxi /> */}
-            <MobilePopup/>
-         </PageLoader>
-        <ScrollToTop/>
-          {/* <FloatingIcons/> */}
-      {/* <Whatsapp /> */}
-    </SmoothScroll>
+            <MobilePopup />
+          </PageLoader>
+          <ScrollToTop />
+          {/* <FloatingIcons /> */}
+          {/* <Whatsapp /> */}
+        </SmoothScroll>
       </body>
     </html>
   );
