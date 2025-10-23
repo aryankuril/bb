ca9455c51273391be0bbb52c82e36d10d0e3f7d6
Aryan Kuril <aryan@bombayblokes.com>
2025-10-23 16:54:33 +0530

    layout added

--- app/components/Career/Firstsection.tsx
+++ app/components/Career/Firstsection.tsx
@@ -63,7 +63,7 @@ const Firstsection = () => {
           initial={{ x: "-120%" }}
           animate={
             isMobile
-              ? { x: 125 }             // 🚫 No movement on mobile
+              ? { x: 140 }             // 🚫 No movement on mobile
               : inView
               ? { x: 550 }           // ✅ Animate on larger screens
               : {}
