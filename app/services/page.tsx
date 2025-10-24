3d75c64172bea1bfe1c30d159374680daa9ae036
jankiBB <167781951+jankiBB@users.noreply.github.com>
2025-10-25 01:28:07 +0530

    client page few icons done

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Firstsection from '../components/Services/Firstsection'
@@ -9,6 +10,11 @@ import Footer from '../components/Footer'
 import Taxi from '../components/Taxi'
 import MobilePopup from '@/app/components/MobilePopup'
 
+
+export const metadata: Metadata = {
+  title: "Website Development Company in Mumbai | Custom, Shopify, WordPress, and more.",
+  description: "Transform your online presence with Bombay Blokes, Mumbai’s best website development company. We design fast, responsive, and conversion-focused designs that leave an impact. Book Your Free Consultation Now.",
+};
 const page = () => {
   return (
     <div>
--- app/services/performance-marketing/page.tsx
+++ app/services/performance-marketing/page.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import Navbar from "../../components/Navbar";
 import FirstSection from "../../components/ServicesInternal/PerformanceMaketing/FirstSection";
 // import SecondSection from "../components/ServicesInternal/PerformanceMaketing/SecondSection";
@@ -10,6 +11,12 @@ import Footer from "../../components/Footer";
 // import ClientScripts from '../components/ClientScripts'
 import Taxi from '../../components/Taxi'
 import MobilePopup from "@/app/components/MobilePopup";
+
+
+export const metadata: Metadata = {
+  title: "Best Performance Marketing Agency in Mumbai | Bombay Blokes",
+  description: "A trusted digital agency for ROI-focused performance marketing. Our tailored strategies are designed to maximize your ROI by boosting clicks, generating leads, and driving sales through data-backed campaigns. Start growing your business today!",
+};
 
 const Index = () => {
   return (
