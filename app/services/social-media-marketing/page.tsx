d2f8cb6b97cc5f612125ab90bdfe980221a2e2b2
jankiBB <167781951+jankiBB@users.noreply.github.com>
2025-10-24 20:44:22 +0530

    test4

---
+++ app/services/social-media-marketing/page.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import Navbar from "../../components/Navbar";
+import FirstSection from "../../components/ServicesInternal/SocialMedia/FirstSection";
+// import SecondSection from "../components/ServicesInternal/SocialMedia/SecondSection";
+import ThirdSection from "../../components/ServicesInternal/SocialMedia/ThirdSection";
+import FourthSection from "../../components/ServicesInternal/SocialMedia/FourthSection";
+import FifthSection from "../../components/ServicesInternal/SocialMedia/FifthSection";
+import SixthSection from "../../components/ServicesInternal/SocialMedia/SixthSection";
+import SeventhSection from "../../components/ServicesInternal/SocialMedia/SeventhSection";
+import Footer from "../../components/Footer";
+// import ClientScripts from '../components/ClientScripts'
+import Taxi from '../../components/Taxi'
+import MobilePopup from "../../components/MobilePopup";
+
+
+export const metadata: Metadata = {
+  title: " Dominate Social Media with Strategies That Spark Engagement",
+  description: "Elevate your brand with social media marketing that captivates and converts. Explore creative campaigns that get noticed!",
+};
+const Index = () => {
+  return (
+    <div>
+      {/* <ClientScripts/> */}
+      {/* all good */}
+       <MobilePopup/>
+      <Taxi/>
+      <Navbar />
+      <FirstSection />
+      {/* <SecondSection /> */}
+      <FourthSection />
+      <ThirdSection />
+      <FifthSection />
+      <SixthSection />
+      <SeventhSection />
+      <Footer />
+    </div>
+  )
+}
+
+export default Index
