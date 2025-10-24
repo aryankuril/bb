87e2c133e3b5de5890cd585c7d13833bc3e50eb1
jankiBB <167781951+jankiBB@users.noreply.github.com>
2025-10-24 18:27:02 +0530

    redirection done

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-
+import type { Metadata } from "next";
 import Navbar from "./components/Navbar";
 import Firstsection from "./components/HomePage/Firstsection";
 import SecondSection from "./components/HomePage/SecondSection";
-import StudioSection from "./components/HomePage/StudioSection"
+import StudioSection from "./components/HomePage/StudioSection";
 import ThirdSection from "./components/HomePage/ThirdSection";
 import WorkCard from "./components/HomePage/WorkCard";
-import PerformanceROI from "./components/HomePage/PerformanceROI"
+import PerformanceROI from "./components/HomePage/PerformanceROI";
 import BombAISection from "./components/HomePage/BombAISection";
 import RubberSection from "./components/HomePage/RubberSection";
 import CardCarousel from "./components/HomePage/CardCarousel";
@@ -14,30 +14,31 @@ import SeventhSection from "./components/ServicesInternal/WebsiteDesign/SeventhS
 import Footer from "./components/Footer";
 import Taxi from "./components/Taxi";
 import MobilePopup from "./components/MobilePopup";
-// import ClientScripts from "./components/ClientScripts";
 
-
+// ✅ Page-specific meta
+export const metadata: Metadata = {
+  title: "Mumbai’s leading Digital Marketing Agency | Bombay Blokes",
+  description: "Looking for a Digital Marketing agency in Mumbai? Want a partner that's dedicated to your success? Choose Bombay Blokes for customized and result-driven Digital Solutions. Visit us now!",
+};
 
 export default function Home() {
   return (
     <div>
-      {/* <ClientScripts/> */}
-      <Taxi/>
-      <MobilePopup/>
+      <Taxi />
+      <MobilePopup />
       <Navbar />
-      <Firstsection/>
-      <SecondSection/>
-      <ThirdSection/>
+      <Firstsection />
+      <SecondSection />
+      <ThirdSection />
       <WorkCard />
-      <StudioSection/>
-      <PerformanceROI/>
-      <BombAISection/>
-      <RubberSection/>
-      <CardCarousel/>
-      <SixthSection/>
-      <SeventhSection/>
+      <StudioSection />
+      <PerformanceROI />
+      <BombAISection />
+      <RubberSection />
+      <CardCarousel />
+      <SixthSection />
+      <SeventhSection />
       <Footer />
     </div>
-    
   );
 }
