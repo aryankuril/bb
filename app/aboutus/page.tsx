be3a451e664776aeae1980dc034744d33f17153b
Aryan Kuril <aryan@bombayblokes.com>
2025-10-30 20:38:13 +0530

    error solve testing

--- app/aboutus/page.tsx
+++ app/aboutus/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Firstsection from '../components/AboutUs/Firstsection'
-// import SecondSection from '../components/AboutUs/SecondSection'
+import SecondSection from '../components/AboutUs/SecondSection'
 import ThirdSection from '../components/AboutUs/ThirdSection'
 // import FourthSection from '../components/AboutUs/FourthSection'
 import SeventhSection from '../components/ServicesInternal/WebsiteDesign/SeventhSection'
@@ -9,7 +9,6 @@ import RubberSection from '../components/HomePage/RubberSection'
 import Footer from '../components/Footer'
 // import ClientScripts from '../components/ClientScripts'
 import Taxi from '../components/Taxi'
-import MobilePopup from '../components/MobilePopup'
 import SmoothScroll from '../components/SmoothScroll'
 import { Metadata } from 'next'
 
@@ -22,12 +21,11 @@ const Index = () => {
   return (
     <div>
         {/* <ClientScripts/> */}
-         {/* <MobilePopup/> */}
          <SmoothScroll>
         <Taxi/>
         <Navbar/>
         <Firstsection />
-        {/* <SecondSection/> */}
+        <SecondSection/>
         <ThirdSection />
         {/* <FourthSection /> */}
         <RubberSection/>
