753ab49e85b39a71ffa0a8635b2825e03ee4623b
Aryan Kuril <aryan@bombayblokes.com>
2025-10-30 20:29:28 +0530

    yess all good to go

--- app/aboutus/page.tsx
+++ app/aboutus/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Firstsection from '../components/AboutUs/Firstsection'
-import SecondSection from '../components/AboutUs/SecondSection'
+// import SecondSection from '../components/AboutUs/SecondSection'
 import ThirdSection from '../components/AboutUs/ThirdSection'
 // import FourthSection from '../components/AboutUs/FourthSection'
 import SeventhSection from '../components/ServicesInternal/WebsiteDesign/SeventhSection'
@@ -27,7 +27,7 @@ const Index = () => {
         <Taxi/>
         <Navbar/>
         <Firstsection />
-        <SecondSection/>
+        {/* <SecondSection/> */}
         <ThirdSection />
         {/* <FourthSection /> */}
         <RubberSection/>
