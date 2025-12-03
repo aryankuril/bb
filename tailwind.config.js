f047aa3f207aa46d686e62d9bcfa57157d32375d
Aryan Kuril <aryan@bombayblokes.com>
2025-12-03 19:16:16 +0530

    next congig full optimised

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
@@ -17,5 +19,6 @@ module.exports = {
       },
     },
   },
+  safelist: [],
   plugins: [],
 };
