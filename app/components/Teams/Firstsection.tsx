9f66515398e75d0e15d296dc618a847458824523
Aryan Kuril <aryan@bombayblokes.com>
2025-12-11 15:32:49 +0530

    reduced zoom

--- app/components/Teams/Firstsection.tsx
+++ app/components/Teams/Firstsection.tsx
@@ -27,7 +27,8 @@ const Firstsection: React.FC = () => {
 
   const fitScale = Math.max(scaleX, scaleY);
 
-  setTargetScale(Math.max(1, fitScale));
+setTargetScale(Math.max(1, fitScale * 0.88));
+
 };
 
   /** 🔥 Calculate correct scale (GSAP-style) */
