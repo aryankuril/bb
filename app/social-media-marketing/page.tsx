1e29b369c04bed84feefac8e3af1f3b44acc9681
Aryan Kuril <aryan@bombayblokes.com>
2026-08-26 15:59:49 +0530

    case study hidden done

--- app/social-media-marketing/page.tsx
+++ app/social-media-marketing/page.tsx
@@ -24,7 +24,7 @@ const Index = () => {
       <LogoMarquee />
       <Services />
       <CreativeWall/>
-      <CaseStudies />
+      {/* <CaseStudies /> */}
       <WhyUs />
       <Process />
       <Testimonials />
